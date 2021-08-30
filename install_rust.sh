ff8b917008298ced818ab0e0718d30d8b58169a1
Linh B Ngo <lngo@wcupa.edu>
2021-08-29 22:49:31 -0400

    Update install_rust.sh

--- install_rust.sh
+++ install_rust.sh
@@ -5,6 +5,7 @@ set -x
 sudo apt-get update -qq --fix-missing 
 sudo apt-get install -qq -y build-essential tmux git gdb wget nano vim sudo wamerican gcc-multilib valgrind
 sudo git clone https://github.com/longld/peda.git
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 while IFS= read -r line; do
   echo "source /peda/peda.py" > /home/${line}/.gdbinit && \
